7795c7d61598189980bc35b5cfaecce4f016c5d1
Silentfeeel <ivanbombin60@gmail.com>
2026-02-09 18:21:02 +0300

    Update params.pyadd

--- params.py
+++ params.py
@@ -3,15 +3,7 @@ from api import API_KEY_GEOCODE, API_KEY_STATIC
 class Params:
     def __init__(self, params: dict):
         self.params = dict()
-        self.default_geocode_params = {
-            "size": "600,450",
-            "ll": "52.043386, 39.736096",
-            "z": "6",
-            "api_key": API_KEY_GEOCODE, 
-        }
-        # self.default_static_params = {
-        #     "1": "1"
-        # }
+        self.default_geocode_params = dict()
         self.add_params(params)
     
     def add_params(self, params: dict):
@@ -29,3 +21,19 @@ class Params:
     
     def get_params(self):
         return self.params
+    
+    def dict2str(self):
+        return "&".join([f"{key}={value}" for key, value in self.params.items()])
+    
+    
+class GeocodeParams(Params):
+    def __init__(self, params):
+        super().__init__(params)
+        self.add_params({"apikey": API_KEY_GEOCODE})
+
+
+class StaticParams(Params):
+    def __init__(self, params):
+        super().__init__(params)
+        self.add_params({"apikey": API_KEY_STATIC})
+        
