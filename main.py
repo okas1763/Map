f57bcb8b10d943e5b0e559bd815e2b8177e9ceb0
dmitrij <fleksikusa@gmail.com>
2026-02-09 16:16:03 +0300

    add map

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import requests
+import arcade
+
+WINDOW_WIDTH = 720
+WINDOW_HEIGHT = 720
+WINDOW_TITLE = "MAP"
+MAP_FILE = "map.png"
+num = int(input('Введите масштаб '))
+x_cord = int(input('Введите х '))
+y_cord = int(input('Введите у '))
+
+class GameView(arcade.Window):
+    def setup(self):
+        self.get_image()
+
+    def on_draw(self):
+        self.clear()
+
+        arcade.draw_texture_rect(
+            self.background,
+            arcade.LBWH(
+                (self.width - self.background.width) // 2,
+                (self.height - self.background.height) // 2,
+                self.background.width,
+                self.background.height
+            ),
+        )
+
+    def get_image(self):
+        server_address = 'https://static-maps.yandex.ru/v1?'
+        api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
+        ll_spn = f'll={x_cord},{y_cord}&spn={num},{num}'
+        # Готовим запрос.
+
+        map_request = f"{server_address}{ll_spn}&apikey={api_key}"
+        response = requests.get(map_request)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        # Запишем полученное изображение в файл.
+        with open(MAP_FILE, "wb") as file:
+            file.write(response.content)
+
+        self.background = arcade.load_texture(MAP_FILE)
+
+
+def main():
+    gameview = GameView(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+    gameview.setup()
+    arcade.run()
+    # Удаляем за собой файл с изображением.
+    os.remove(MAP_FILE)
+
+
+if __name__ == "__main__":
+    main()
