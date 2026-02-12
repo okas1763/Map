c700f81ec29cb046e48c6b6165b8f1f56809f252
korobka170111-afk <korobka170111@gmail.com>
2026-02-12 18:25:38 +0300

    Update defs.py

--- defs.py
+++ defs.py
@@ -2,8 +2,16 @@ import requests
 from params import main_config
 
 def minus_spn():
-    main_config["spn"] = main_config["spn"] - 0.05
+    main_config["spn"] = main_config["spn"] - 0.1
 
 
 def plus_spn():
-    main_config["spn"] = main_config["spn"] + 0.05
+    main_config["spn"] = main_config["spn"] + 0.1
+
+
+def minus_x():
+    main_config["x"] = main_config["x"] - 0.1
+
+
+def plus_x():
+    main_config["x"] = main_config["x"] + 0.1
